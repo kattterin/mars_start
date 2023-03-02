a3747b7459c5e619faec25203a70018135100939
kattterina_i <katerina9katya@gmail.com>
2023-03-02 15:43:58 +0300

    add_promotion_image

--- main.py
+++ main.py
@@ -65,5 +65,32 @@ def image_mars():
                     </html>"""
 
 
+@app.route('/promotion_image')
+def promotion_image():
+    return f"""<!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                        <link rel="stylesheet"
+                        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                        crossorigin="anonymous">
+                        <title>Реклама с картинкой</title>
+                      </head>
+                      <body>
+                        <h1>Жди нас, Марс!</h1>
+                        <img src="{url_for('static', filename='img/mars.png')}" 
+                            width="300" height="300" 
+                            alt="здесь должна была быть картинка, но не нашлась">
+                        <div class="alert alert-secondary" role="alert">Человечество вырастает из детства.</div>
+                        <div class="alert alert-success" role="alert">Человечеству мала одна планета.</div>
+                        <div class="alert alert-secondary" role="alert">Мы сделаем обитаемыми безжизненные пока планеты.</div>
+                        <div class="alert alert-warning" role="alert">И начнем с Марса!</div>
+                        <div class="alert alert-danger" role="alert">Присоединяйся!</div>
+                      </body>
+                    </html>"""
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
