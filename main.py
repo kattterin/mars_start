cbc5392de7cfb412f86486740c12aa432f599174
kattterina_i <katerina9katya@gmail.com>
2023-03-02 17:21:30 +0300

    add_results

--- main.py
+++ main.py
@@ -65,6 +65,29 @@ def image_mars():
                     </html>"""
 
 
+@app.route('/results/<nickname>/<int:level>/<float:rating>')
+def results(nickname, level, rating):
+    return f"""<!doctype html>
+                     <html lang="en">
+                       <head>
+                         <meta charset="utf-8">
+                         <link rel="stylesheet"
+                         href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                         integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                         crossorigin="anonymous">
+                         <title>Предложение</title>
+                       </head>
+                       <body>
+                         <h1>Результаты отбора</h1>
+                         <h3>Претендента на участие в миссии {nickname}:</h3>
+                         <div class="alert alert-success" role="alert">Поздравляем! Ваш рейтинг после {level} этапа отбора</div>
+                         <div class="alert alert-light" role="alert">составляет {rating}</div>
+                         <div class="alert alert-warning" role="alert">Желаем удачи!</div>
+  
+                       </body>
+                     </html>"""
+
+
 @app.route('/choice/<planet_name>')
 def offer(planet_name):
     planets = {'Меркурий': ["Это самая маленькая планета солнечной системы;", "Самая близкая к Солнцу;",
