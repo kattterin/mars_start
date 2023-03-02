8757225119880669a20a3df85097c67201b8ce49
kattterina_i <katerina9katya@gmail.com>
2023-03-02 21:39:24 +0300

    add_login_and_answer

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from flask import Flask, url_for, request, render_template
+from flask import Flask, url_for, request, render_template, redirect
+from login_form import LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 @app.route('/training/<prof>')
@@ -12,6 +14,28 @@ def training(prof):
 @app.route('/index/<title>')
 def index(title="Миссия на марс"):
     return render_template('index.html', title=title)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/')
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/answer')
+@app.route('/auto_answer')
+def answer():
+    param = {"title": "Автоматический ответ",
+             "surname": "Watny",
+             "name": "Mark",
+             "education": "выше среднего",
+             "profession": "штурман марсохода",
+             "sex": "male",
+             "motivation": "Всегда мечтал застрять на Марсе!",
+             "ready": "True"}
+    return render_template("auto_answer.html", **param)
 
 
 # @app.route('/')
@@ -179,123 +203,6 @@ def promotion_image():
                         <div class="alert alert-danger" role="alert">Присоединяйся!</div>
                       </body>
                     </html>"""
-
-
-@app.route('/form_sample', methods=['POST', 'GET'])
-def form_sample():
-    if request.method == 'GET':
-        return f'''<!doctype html>
-                        <html lang="en">
-                          <head>
-                            <meta charset="utf-8">
-                            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                            <link rel="stylesheet"
-                            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
-                            integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
-                            crossorigin="anonymous">
-                            <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
-                            <title>Отбор астронавтов</title>
-                          </head>
-                          <body>
-                            <h1 align="center">Анкета претендента</h1>
-                            <h2 align="center">на участие в миссии</h2>
-                            <div>
-                                <form class="login_form" method="post">
-                                    <input type="text" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Введите фамилию" name="surname">
-                                    <input type="text" class="form-control" id="password" placeholder="Введите имя" name="name">
-                                    <p></p>
-                                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Введите адрес почты" name="email">
-                                    <div class="form-group">
-                                        <label for="educationSelect">Какое у Вас образование</label>
-                                        <select class="form-control" id="educationSelect" name="education">
-                                          <option>Начальное</option>
-                                          <option>Основное</option>
-                                          <option>Среднее</option>
-                                          <option>Среднее профессиональное</option>
-                                          <option>Высшее</option>
-                                        </select>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="professionSelect">Какие у Вас профессии</label>
-                                        <div>
-                                            <input type="checkbox" id="in-is" name="in-is" checked>
-                                            <label for="in-is">Инженер-исследователь</label>
-                                        </div>
-
-                                        <div>
-                                          <input type="checkbox" id="pilot" name="pilot">
-                                          <label for="pilot">Пилот</label>
-                                        </div>
-
-                                        <div>
-                                          <input type="checkbox" id="climat" name="climat">
-                                          <label for="climat">Климатолог</label>
-                                        </div>
-
-                                        <div>
-                                          <input type="checkbox" id="doctor" name="doctor">
-                                          <label for="doctor">Врач</label>
-                                        </div>
-
-                                        <div>
-                                          <input type="checkbox" id="builder" name="builder">
-                                          <label for="builder">Строитель</label>
-                                        </div>
-
-                                        <div>
-                                          <input type="checkbox" id="exobio" name="exobio">
-                                          <label for="exobio">Экзобиолог</label>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="form-check">Укажите пол</label>
-                                        <div class="form-check">
-                                          <input class="form-check-input" type="radio" name="sex" id="male" value="male" checked>
-                                          <label class="form-check-label" for="male">
-                                            Мужской
-                                          </label>
-                                        </div>
-                                        <div class="form-check">
-                                          <input class="form-check-input" type="radio" name="sex" id="female" value="female">
-                                          <label class="form-check-label" for="female">
-                                            Женский
-                                          </label>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="quest">Почему вы хотите принять участие в миссии?</label>
-                                        <textarea class="form-control" id="quest" rows="3" name="quest"></textarea>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="photo">Приложите фотографию</label>
-                                        <input type="file" class="form-control-file" id="photo" name="file">
-                                    </div>
-
-                                    <div class="form-group form-check">
-                                        <input type="checkbox" class="form-check-input" id="acceptRules" name="accept">
-                                        <label class="form-check-label" for="acceptRules">Готовы ли остаться на Марсе?</label>
-                                    </div>
-                                    <button type="submit" class="btn btn-primary">Записаться</button>
-                                </form>
-                            </div>
-                          </body>
-                        </html>'''
-    elif request.method == 'POST':
-        print(request.form.get('surname'))
-        print(request.form.get('name'))
-        print(request.form.get('email'))
-        print(request.form.get('education'))
-        print(request.form.get('in-is', 'off'))
-        print(request.form.get('pilot', 'off'))
-        print(request.form.get('climat', 'off'))
-        print(request.form.get('doctor', 'off'))
-        print(request.form.get('builder', 'off'))
-        print(request.form.get('exobio', 'off'))
-        print(request.form.get('sex'))
-        print(request.form.get('quest'))
-        print(request.form.get('file'))
-        print(request.form.get('accept'))
-        return "<h1>Форма отправлена<h1>"
 
 
 if __name__ == '__main__':
